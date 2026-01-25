9f46fd380669c350e59667946b3f97c793d499eb
NATTHAPHOL <natthapholsaeaueng@gmail.com>
2026-01-25 18:48:47 +0700

    แก้ครั้งที่ 2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // อนุญาตให้เข้าถึงจาก ngrok หรือ domain ภายนอกได้
+    allowedHosts: true
+  }
 })
